01198ac5758af11e0026c5ac95c12c0f199433f6
Latzy47 <derfaule47@googlemail.com>
2020-04-24 05:42:21 +0200

    commit

--- mod_test.py
+++ mod_test.py
@@ -10,7 +10,8 @@ from adisp import async, process
 from messenger.proto.events import g_messengerEvents
 from messenger.proto.interfaces import IChatError
 
-mod_toggle = 3  # [0,1,2,3] für [aus, only arty, only HE, HE + teamBL]
+mod_toggle = {'aus': 0, 'only arty': 1, 'only HE': 2, 'HE + teamBL': 3}
+_mod_toggle = mod_toggle['HE + teamBL']  # [0,1,2,3] für [aus, only arty, only HE, HE + teamBL]
 # mit Config Datei können Zustände bleiben und sind nicht bei jedem Spielstart wieder auf Default
 
 check_running = False
@@ -55,31 +56,33 @@ def sendMessage(message, types):
 
 def key_events_():
     global mod_toggle
+    global _mod_toggle
     old_handler = game.handleKeyEvent
 
     def new_handler(event):
         global mod_toggle
         global check_running
+        global _mod_toggle
         isDown, key, mods, isRepeat = game.convertKeyEvent(event)
         if isDown and mods == 4 and key == Keys.KEY_O:
-            mod_toggle += 1
-            if mod_toggle > 3:
-                mod_toggle = 0
-            if mod_toggle == 0:
+            _mod_toggle += 1
+            if _mod_toggle > 3:
+                _mod_toggle = 0
+            if _mod_toggle == mod_toggle['aus']:
                 sendMessage("Mod disabled", SystemMessages.SM_TYPE.Warning)
-            elif mod_toggle == 1:
+            elif _mod_toggle == mod_toggle['only arty']:
                 sendMessage("Only Cancer", SystemMessages.SM_TYPE.Warning)
-            elif mod_toggle == 2:
+            elif _mod_toggle == mod_toggle['only HE']:
                 sendMessage("Only HE", SystemMessages.SM_TYPE.Warning)
             else:
                 sendMessage("HE + blacklist Teams", SystemMessages.SM_TYPE.Warning)
-        if mod_toggle == 1:
+        if _mod_toggle == mod_toggle['only arty']:
             pass  # funktion auto arty einfügen
             if isDown and mods == 4 and key == Keys.KEY_B:
                 sendMessage("arty mit key", SystemMessages.SM_TYPE.Warning)  # funktion arty mit key
-        elif mod_toggle == 2:
+        elif _mod_toggle == mod_toggle['only HE']:
             pass  # funktion auto HE einfügen
-        elif mod_toggle == 3:
+        elif _mod_toggle == mod_toggle['HE + teamBL']:
             pass  # funktion auto HE einfügen
             if isDown and mods == 4 and key == Keys.KEY_B:
                 g_messengerEvents.onErrorReceived(MyError())
