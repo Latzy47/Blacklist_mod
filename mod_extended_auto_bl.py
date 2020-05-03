2655a5de5519554fb9fbf44843a0c5b039012623
Latzy47 <derfaule47@googlemail.com>
2020-05-03 22:47:35 +0200

    commit

--- mod_extended_auto_bl.py
+++ mod_extended_auto_bl.py
@@ -173,36 +173,38 @@ def teambl_key():
     global check_running
     prebID = 0
     check_running = True
-    sessionProvider = dependency.instance(IBattleSessionProvider)
-    setup = repositories.BattleSessionSetup(avatar=BigWorld.player(), sessionProvider=sessionProvider)
-    adding = anonymizer_fakes_ctrl.AnonymizerFakesController(setup)
-    databID = getAvatarDatabaseID()
     arena = getattr(BigWorld.player(), 'arena', None)
-    vehID = getattr(BigWorld.player(), 'playerVehicleID', None)
-    if vehID is not None and vehID in arena.vehicles:
-        prebID = arena.vehicles[vehID]['prebattleID']
+    if arena is not None:
+        sessionProvider = dependency.instance(IBattleSessionProvider)
+        setup = repositories.BattleSessionSetup(avatar=BigWorld.player(), sessionProvider=sessionProvider)
+        adding = anonymizer_fakes_ctrl.AnonymizerFakesController(setup)
+        databID = getAvatarDatabaseID()
 
-    for (vehicleID, vData) in getArena().vehicles.iteritems():
-        databaseID = vData['accountDBID']
-        av_ses_id = vData['avatarSessionID']
-        _prebattleID = vData['prebattleID']
-        user = adding.usersStorage.getUser(av_ses_id, scope=UserEntityScope.BATTLE)
-        if user is not None:
-            if databaseID != databID and not (user.isFriend() or user.isIgnored()):
-                if prebID > 0 and prebID != _prebattleID:
-                    adding.addBattleIgnored(av_ses_id)
-                    yield wait(1.1)
-                elif prebID == 0:
-                    adding.addBattleIgnored(av_ses_id)
-                    yield wait(1.1)
-        else:
-            if databaseID != databID:
-                if prebID > 0 and prebID != _prebattleID:
-                    adding.addBattleIgnored(av_ses_id)
-                    yield wait(1.1)
-                elif prebID == 0:
-                    adding.addBattleIgnored(av_ses_id)
-                    yield wait(1.1)
+        vehID = getattr(BigWorld.player(), 'playerVehicleID', None)
+        if vehID is not None and vehID in arena.vehicles:
+            prebID = arena.vehicles[vehID]['prebattleID']
+
+        for (vehicleID, vData) in getArena().vehicles.iteritems():
+            databaseID = vData['accountDBID']
+            av_ses_id = vData['avatarSessionID']
+            _prebattleID = vData['prebattleID']
+            user = adding.usersStorage.getUser(av_ses_id, scope=UserEntityScope.BATTLE)
+            if user is not None:
+                if databaseID != databID and not (user.isFriend() or user.isIgnored()):
+                    if prebID > 0 and prebID != _prebattleID:
+                        adding.addBattleIgnored(av_ses_id)
+                        yield wait(1.1)
+                    elif prebID == 0:
+                        adding.addBattleIgnored(av_ses_id)
+                        yield wait(1.1)
+            else:
+                if databaseID != databID:
+                    if prebID > 0 and prebID != _prebattleID:
+                        adding.addBattleIgnored(av_ses_id)
+                        yield wait(1.1)
+                    elif prebID == 0:
+                        adding.addBattleIgnored(av_ses_id)
+                        yield wait(1.1)
     check_running = False
 
 
