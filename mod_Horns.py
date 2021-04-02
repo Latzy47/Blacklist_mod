ba8f8f2d05ce1b788b816d02f913ba75c0f1216d
Latzy47 <derfaule47@googlemail.com>
2021-04-02 14:29:23 +0200

    preparation before merging

--- mod_Horns.py
+++ mod_Horns.py
@@ -12,55 +12,50 @@ class ConfigInterface(PYmodsConfigInterface):
 
     def init(self):
         self.ID = 'X_Auto_BL'
-        self.version = '1.31 (%(file_compile_date)s)'
-        self.defaultKeys = {'hotkey1': [Keys.KEY_O, [Keys.KEY_LALT, Keys.KEY_RALT]],
-                            'hotkey2': [Keys.KEY_B, [Keys.KEY_LALT, Keys.KEY_RALT]],
-                            'hotkey3': [Keys.KEY_O, [Keys.KEY_LCONTROL, Keys.KEY_RCONTROL]],  # useless?
-                            'hotkey4': [Keys.KEY_C, [Keys.KEY_LALT, Keys.KEY_RALT]],
-                            'hotkey5': [Keys.KEY_X, [Keys.KEY_LALT, Keys.KEY_RALT]]}
+        self.version = '1.31 (01.04.2021)'
+        self.defaultKeys = {'hotkey1': [Keys.KEY_O, [Keys.KEY_LALT, Keys.KEY_RALT]],  # switch modes
+                            'hotkey2': [Keys.KEY_B, [Keys.KEY_LALT, Keys.KEY_RALT]],  # key function
+                            'hotkey3': [Keys.KEY_C, [Keys.KEY_LALT, Keys.KEY_RALT]],  # enable clear
+                            'hotkey4': [Keys.KEY_X, [Keys.KEY_LALT, Keys.KEY_RALT]]}  # start clear
         self.data = {'enabled': True,
-                     'mode': 3,
                      'ignored': 1000000,
                      'extended': True,
                      'friends': 1000000,
                      'hotkey1': self.defaultKeys['hotkey1'],
                      'hotkey2': self.defaultKeys['hotkey2'],
                      'hotkey3': self.defaultKeys['hotkey3'],
-                     'hotkey4': self.defaultKeys['hotkey4'],
-                     'hotkey5': self.defaultKeys['hotkey5']}
+                     'hotkey4': self.defaultKeys['hotkey4']}
         self.i18n = {
             'UI_description': 'Extended automated blacklist',
-            'UI_setting_mode_text': 'Current mode',
-            'UI_setting_mode_tooltip': 'This selects and shows the current mode.',
             'UI_setting_ignored_text': 'Number blacklist',
-            'UI_setting_ignored_tooltip': 'This number shows how big your blacklist is.',
+            'UI_setting_ignored_tooltip': 'This number shows the length of your blacklist.',
             'UI_setting_extended_text': 'Extend lists',
-            'UI_setting_extended_tooltip': 'This enables/disables the extention of the blacklist and friendlist.\n'
+            'UI_setting_extended_tooltip': 'This enables/disables the extention of the blacklist'
+                                           ' and friendlist.\n'
                                            'Restarting the game is required to take effect.',
             'UI_setting_friends_text': 'Number friendlist',
-            'UI_setting_friends_tooltip': 'This number shows how big your friendlist is.',
-            'UI_setting_hotkey1_text': 'Hotkey',
-            'UI_setting_hotkey1_tooltip': 'This is a hotkey.',
-            'UI_setting_hotkey2_text': 'Hotkey',
-            'UI_setting_hotkey2_tooltip': 'This is a hotkey.',
-            'UI_setting_hotkey3_text': 'Hotkey',
-            'UI_setting_hotkey3_tooltip': 'This is a hotkey.',
-            'UI_setting_hotkey4_text': 'Hotkey',
-            'UI_setting_hotkey4_tooltip': 'This is a hotkey.',
-            'UI_setting_hotkey5_text': 'Hotkey',
-            'UI_setting_hotkey5_tooltip': 'This is a hotkey.'}
+            'UI_setting_friends_tooltip': 'This number shows the length of your friendlist.',
+            'UI_setting_hotkey1_text': 'Switch modes',
+            'UI_setting_hotkey1_tooltip': 'Pressing this hotkey will switch between modes.',
+            'UI_setting_hotkey2_text': 'Blacklist key',
+            'UI_setting_hotkey2_tooltip': 'Pressing this hotkey will blacklist your desired tanks.',
+            'UI_setting_hotkey3_text': 'Clear enable',
+            'UI_setting_hotkey3_tooltip': 'Pressing this hotkey will enable/disable clearing your blacklist.',
+            'UI_setting_hotkey4_text': 'Clear start',
+            'UI_setting_hotkey4_tooltip': 'Pressing this hotkey will start clearing your blacklist.'}
         super(ConfigInterface, self).init()
         self.containerClass = MyPYmodsSettingContainer
+        self.modSettingsID = 'x_auto_bl'
+        self.modsGroup = 'xAutoBl'
+        self.author = 'by FvckingLatzyMan'
 
     def createTemplate(self):
         return {'modDisplayName': self.i18n['UI_description'],
                 'enabled': self.data['enabled'],
                 'column1': [self.tb.createSlider('ignored', 5000, 1000000, 5000),
                             self.tb.createSlider('friends', 5000, 1000000, 5000),
-                            self.tb.createHotKey('hotkey1'),
-                            self.tb.createHotKey('hotkey3')],
+                            self.tb.createHotKey('hotkey1')],
                 'column2': [self.tb.createControl('extended'),
-                            self.tb.createOptions('mode', [0, 1, 2, 3]),
                             self.tb.createHotKey('hotkey2'),
                             self.tb.createHotKey('hotkey4'),
                             self.tb.createHotKey('hotkey5')]}
